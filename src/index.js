f09589b99e771d92044918face901a69c2bd6dcf
henrylong <henrylong719@gmail.com>
2021-12-05 20:07:57 +1030

    add css loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert('HELLO FROM WEBPACK!');
+import { run } from './app/app';
+import './main.css';
+import { AlertService } from './app/alert.service';
+import { ComponentService } from './app/component.service';
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
